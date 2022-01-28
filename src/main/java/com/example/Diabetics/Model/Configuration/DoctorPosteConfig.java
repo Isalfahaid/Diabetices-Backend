e0a8ec999fc62767ba623539d8fd48c46597418b
SalmaAlfahaid <91747841+Isalfahaid@users.noreply.github.com>
2022-01-28 17:10:18 +0300

    Update DoctorPosteConfig.java

--- src/main/java/com/example/Diabetics/Model/Configuration/DoctorPosteConfig.java
+++ src/main/java/com/example/Diabetics/Model/Configuration/DoctorPosteConfig.java
@@ -10,13 +10,13 @@ import java.util.List;
 
 @Configuration
 public class DoctorPosteConfig {
-    @Bean
-    CommandLineRunner ccc(DoctorPosteRepository repository) {
-        return args -> {
-            DoctorPoste salma = new DoctorPoste(1,"https://media.istockphoto.com/photos/pediatrician-talks-with-patients-mother-picture-id1293681788?b=1&k=20&m=1293681788&s=170667a&w=0&h=86FIfZB_E9W0-1olORv2I6P5Fvd6EUMZUVPyiMfVrAY=" ,"Doctor","Diabeties");
-            DoctorPoste s = new DoctorPoste(2,"https://media.istockphoto.com/photos/pediatrician-talks-with-patients-mother-picture-id1293681788?b=1&k=20&m=1293681788&s=170667a&w=0&h=86FIfZB_E9W0-1olORv2I6P5Fvd6EUMZUVPyiMfVrAY=" ,"Doctor","Diabeties");
+//     @Bean
+//     CommandLineRunner ccc(DoctorPosteRepository repository) {
+//         return args -> {
+//             DoctorPoste salma = new DoctorPoste(1,"https://media.istockphoto.com/photos/pediatrician-talks-with-patients-mother-picture-id1293681788?b=1&k=20&m=1293681788&s=170667a&w=0&h=86FIfZB_E9W0-1olORv2I6P5Fvd6EUMZUVPyiMfVrAY=" ,"Doctor","Diabeties");
+//             DoctorPoste s = new DoctorPoste(2,"https://media.istockphoto.com/photos/pediatrician-talks-with-patients-mother-picture-id1293681788?b=1&k=20&m=1293681788&s=170667a&w=0&h=86FIfZB_E9W0-1olORv2I6P5Fvd6EUMZUVPyiMfVrAY=" ,"Doctor","Diabeties");
 
-            repository.saveAll(List.of(salma,s));
-        };
-        }
+//             repository.saveAll(List.of(salma,s));
+//         };
+//         }
 }
